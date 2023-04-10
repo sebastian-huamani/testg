6c54ebabf4dd6c14381e954eead6c4479a4d874a
sebastian-huamani <huamanitassar@mail.com>
2023-04-10 16:44:37 -0500

    base

---
+++ app/Http/Controllers/GaseosaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\gaseosa;
+use Illuminate\Http\Request;
+
+class GaseosaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(gaseosa $gaseosa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(gaseosa $gaseosa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, gaseosa $gaseosa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(gaseosa $gaseosa)
+    {
+        //
+    }
+}
